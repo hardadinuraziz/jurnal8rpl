e8c5405a9c7ba07ced394cb7ff5b64a7732b255f
Hardadi Nur Aziz <hardadinuraziz@gmail.com>
2017-04-06 09:28:20 +0700

    Update nomor1.java

--- nomor1.java
+++ nomor1.java
@@ -1,6 +1,23 @@
-public class nomor1 {
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
 
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
+        
         Scanner show = new Scanner(System.in);
         System.out.print("Masukan Jumlah Deret Fibonacci ");
         int n = show.nextInt();
@@ -18,4 +35,8 @@ public class nomor1 {
         }
     }
     }
+    
+    
+
+
     
